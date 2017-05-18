7d8c8faf337fed89f7b728643defe7be9306fe1f
bezotic <RichardDeLosSantos1292@gmail.com>
2017-05-18 15:18:52 -0500

    pushing files

--- public/js/planet-string.js
+++ public/js/planet-string.js
@@ -23,6 +23,22 @@
           
 
      });
+   function oddNumbers() {
+     for (var n = 1; n <= 20; n++) {
+        var randomNum = Math.floor(Math.random() * 100) + 1;
+        if (randomNum % 3 == 0) {
+            console.log(randomNum + " is odd ");
+        } 
+     }
+    }
+
+    oddNumbers();
+
+    var works = ['pizza', 'cheese', 'cake'];
+    var alphabetOrder = works.sort();
+    console.log(alphabetOrder)
+    var commaInBetween = alphabetOrder.split(',');
+    console.log(commaInBetween);
 
     // Bonus: Create a second string that would display your planets in an undordered list.
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
